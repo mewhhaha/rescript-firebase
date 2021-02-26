54e8fb0e23ff03e9f6a128e499d916a2e7584912
Jacob Torrång <kopatheonlyone@hotmail.com>
2021-02-26 19:57:42 +0100

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,9 +1,9 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyCR67Lz9wkDXI57vXN8GtC3csfqZELirbM",
-    authDomain: "fir-demo-81b61.firebaseapp.com",
-    databaseURL: "https://fir-demo-81b61.firebaseio.com",
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
     projectId: "fir-demo-81b61",
-    storageBucket: "fir-demo-81b61.appspot.com",
-    messagingSenderId: "863334807998",
-    appId: "1:863334807998:web:350a0e6af5f4caaf3ae1ef",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
   };
